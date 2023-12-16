ef6877f6c7ba87adc6aeab4daa14974127bd60a7
unknown <weabuddin@gmail.com>
2023-12-16 12:54:24 +0700

    change back-end route

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://weather-api-tau-six.vercel.app/',
-  BACK_END: 'http://localhost:3000/',
+  BACK_END: 'https://agrofarm-backend-udondondodons-projects.vercel.app/',
   DATABASE_NAME: 'Weather-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'weather',
