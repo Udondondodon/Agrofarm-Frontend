8cbd9ae83460db70c7e2ecddba21b625f7c07e18
unknown <weabuddin@gmail.com>
2023-12-17 19:56:38 +0700

    cobaan

--- src/scripts/components/artikel.js
+++ src/scripts/components/artikel.js
@@ -5,40 +5,20 @@ class Artikel extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <!-- Artikel Section -->
-<section id="artikel" class="artikel">
-  <h2>Artikel</h2>
-
-  <div class="artikel-card">
-    <div class="artikel-image">
-      <img src="img/artikel1.jpg" alt="Artikel 1" />
+    <div class="artikel-container">
+      <div class="artikel-img">
+        <img src="../img/artikel.jpg" alt="AgroFarm Image" class="artimg">
+      </div>
+      <div class="artikel-desc">
+        <h1>Artikel</h1>
+        <p>
+          Kunci pertanian yang subur adalah dengan ilmu pengetahuan, dengan itu hasil panen dapat berpengaruh dengan cara-cara yang benar. 
+          Ingin tahu bagaimana cara bertani yang benar agar menghasilkan hasil yang memuaskan?
+        </p>
+        <button class="btn-2"><a href="#/artikel">Yuk Lihat Disini!</a></button>
+      </div>
     </div>
-    <div class="artikel-content">
-      <h3>Judul Artikel 1</h3>
-      <p>Tanggal: 1 Januari 2023</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-      <a href="#" class="btn">Baca Selengkapnya</a>
-    </div>
-  </div>
-
-  <div class="artikel-card">
-    <div class="artikel-image">
-      <img src="img/artikel2.jpg" alt="Artikel 2" />
-    </div>
-    <div class="artikel-content">
-      <h3>Judul Artikel 2</h3>
-      <p>Tanggal: 5 Februari 2023</p>
-      <p>
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-      <a href="#" class="btn">Baca Selengkapnya</a>
-    </div>
-  </div>
-</section>
-
-        `;
+    `;
   }
 }
 
--- src/scripts/components/pencatatan.js
+++ src/scripts/components/pencatatan.js
@@ -5,11 +5,19 @@ class Pencatatan extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <section id="" class="">
-    <h2>Pencatatan Hasil Panen</h2>
+    <div class="pencatat">
+      <div class="pencatat-img">
+        <img src="../img/catat.jpg" alt="AgroFarm Image" class="image">
+      </div>
+      <div class="pencatat-content">
+        <h1>Pencatatan Hasil Panen</h1>
+        <p>
+          Masih bingung untuk mencatat hasil panen yang kamu dapatkan? Tenang, AgroFarm memiliki fitur Pencatatan kamu bisa coba disini.
+        </p>
+        <button class="btn-1"><a href="#/pencatatan">Yuk Coba!</a></button>
+      </div>
     </div>
-  </section>
-            `;
+  `;
   }
 }
 
